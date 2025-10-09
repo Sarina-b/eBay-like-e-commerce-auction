235d727f8a8ca551732490c01aae55f8e0817a62
Sarina babadi <sarinababadi900@gmail.com>
2025-10-09 23:22:51 +0330

    Super User is created

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from auctions.models import User, List_Auctions, Watchlist, Watchlist_Items, Comment, Bids
+
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(List_Auctions)
+admin.site.register(Watchlist)
+admin.site.register(Watchlist_Items)
+admin.site.register(Comment)
+admin.site.register(Bids)
